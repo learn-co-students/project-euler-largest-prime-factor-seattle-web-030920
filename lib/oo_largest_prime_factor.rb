ed3a5319e98062f351a5cf3e93e77d2619c90795
fosterv2 <fosterv2@outlook.com>
2020-08-24 12:50:03 -0700

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,17 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    def initialize(input)
+        @input = input
+    end
+
+    def number
+        prime = @input
+        (2..Math.sqrt(@input).to_i).each do |num|
+            break if prime <= 1
+            while prime > num && prime % num == 0
+                prime = prime / num
+            end
+        end
+        prime
+    end
+end
